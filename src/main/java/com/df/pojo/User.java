9ab3d737d50ddc55e0a929ad58f1d38f46e49cc2
MFine <1737374338@qq.com>
2021-01-05 00:04:13 +0800

    更改用户表

--- src/main/java/com/df/pojo/User.java
+++ src/main/java/com/df/pojo/User.java
@@ -1,27 +1,32 @@
 package com.df.pojo;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
  * @author MFine
- */
+ * @version 1.0
+ * @date 2021/1/5 0:02
+ **/
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ApiModel("用户基本信息")
 public class User {
-
-    @ApiModelProperty(example = "1",notes = "可以不写这个属性")
     private Integer id;
 
-    @ApiModelProperty(example = "123",required = true)
     private String password;
 
-    @ApiModelProperty(example = "admin",required = true)
     private String name;
 
+    private String phone;
+
+    private String email;
+
+    private String roleId;
+
+    private Long createTime;
+
+    private Integer v;
 }
