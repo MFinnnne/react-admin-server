fb955567b678ab662b98491e91f894b148b48304
MFine <1737374338@qq.com>
2021-03-02 23:18:51 +0800

    页面访问次数功能进行中

--- src/main/java/com/df/controller/UserController.java
+++ src/main/java/com/df/controller/UserController.java
@@ -78,4 +78,5 @@ public class UserController {
         return ResponseEntity.notFound().build();
     }
 
+
 }
---
+++ src/main/java/com/df/interceptor/RedisUrlCountInterceptor.java
@@ -0,0 +1,29 @@
+package com.df.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author MFine
+ * @version 1.0
+ * @date 2021/3/2 21:41
+ **/
+@Component
+public class RedisUrlCountInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String uri = request.getRequestURI();
+        stringRedisTemplate.opsForValue().increment(uri);
+        System.out.println("哈哈哈哈啊哈");
+        return true;
+    }
+}
