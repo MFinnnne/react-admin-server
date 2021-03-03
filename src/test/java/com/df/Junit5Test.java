7009aa597635d0c82409cacb68d56689a61ffb07
MFine <1737374338@qq.com>
2021-03-03 23:46:42 +0800

    测试

---
+++ src/test/java/com/df/Junit5Test.java
@@ -0,0 +1,20 @@
+package com.df;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * @author MFine
+ * @version 1.0
+ * @date 2021/3/3 23:31
+ **/
+
+public class Junit5Test {
+
+    @Test
+    void test() {
+        Assertions.assertEquals(1, 1);
+
+    }
+}
