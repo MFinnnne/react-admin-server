15bb38884b25728a57a72ee5c964825df476e4d7
MFine <1737374338@qq.com>
2021-07-29 20:50:34 +0800

    Update StorageProperties.java

--- src/main/java/com/df/uploadfiles/storage/StorageProperties.java
+++ src/main/java/com/df/uploadfiles/storage/StorageProperties.java
@@ -11,7 +11,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("storage")
 public class StorageProperties {
 
-    private String location = "upload-dir";
+    private String location = "./upload-dir";
 
     public String getLocation() {
         return location;
